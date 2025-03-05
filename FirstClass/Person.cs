378f50b21d27e9f8c1348cd68186b7a01da82e37
mehmetfatihinan <mehmetfatihinan@gmail.com>
2025-03-05 16:48:00 +0300

    Add project files.

---
+++ FirstClass/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {SurName}, Birthdate: {Birthday}");
+        }
+
+        
+
+    }
+}
---
+++ FirstClass/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FirstClass
+{
+    class FirstClass
+    {
+        static void Main(string[] args)
+        {
+
+            Person student1 = new Person {
+
+                //student1
+                Name = "Ahmet",
+                SurName = "Yılmaz",
+                Birthday = new DateTime(1990, 1, 1)
+
+            };
+
+            Person student2 = new Person {
+                //student2
+                Name = "Mehmet",
+                SurName = "Yılmaz",
+                Birthday = new DateTime(1990, 1, 1)
+
+            };
+
+            Person teacher1 = new Person
+            {
+                //teacher1
+                Name = "Ayşe",
+                SurName = "Yılmaz",
+                Birthday = new DateTime(1990, 1, 1)
+            };
+
+            teacher1.Print();
+            student1.Print();
+            student2.Print();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
